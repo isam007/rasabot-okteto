78ec93811eeac53f809c2b8d9cdc7d2d1c5fb01e
JunioTech <juniotech@gmail.com>
2023-09-13 21:46:58 -0600

    Initial commit

---
+++ chatbot/static/js/constants.js
@@ -0,0 +1,4 @@
+const action_name = "action_hello_world";
+// const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const rasa_server_url = "https://rasa-isam007.cloud.okteto.net/webhooks/rest/webhook";
+const sender_id = uuidv4();
